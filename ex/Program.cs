4648365bf8aa66aa731a75b754090ed75d019c32
Ekaterina Anikina <reityan97@mail.ru>
2022-07-21 21:43:24 +0300

    Решение задачи 25

---
+++ ex/Program.cs
@@ -0,0 +1,23 @@
+﻿void Ex_001()
+{
+
+//Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+
+Console.WriteLine ("Введите число A:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите число B:");
+int B = Convert.ToInt32(Console.ReadLine());
+int count=1;
+int result=1;
+
+for (int i = 1; count <= B; count++)
+{
+    i*=A;
+    result=i;
+}
+
+Console.WriteLine($"{A} в степени {B} = {result}");
+}
+
+Ex_001();
